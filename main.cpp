5daf5b3ca08243ac74ecb304a85a499dca2dda6a
RMalinowski <r.malinowski3211@gmail.com>
2024-12-18 12:52:31 +0100

    feature: handle files opening

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <unordered_map>
 
@@ -6,49 +7,67 @@
 #include "./src/headers/logicGates.h"
 
 int main(){
-    LogicSystem logicSystem;
 
-    ENTRY* input1 = new ENTRY(1, State::HIGH, {4, 5, 6});
-    ENTRY* input2 = new ENTRY(2, State::HIGH, {4});
-    ENTRY* input3 = new ENTRY(3, State::LOW, {5, 6});
 
-    AND* node1 = new AND(4, {7});
-    OR* node2 = new OR(5, {7});
-    XOR* node3 = new XOR(6, {});
-    OR* node4 = new OR(7, {8});
-    NOT* node5 = new NOT(8, {});
+    std::ifstream graph_structure_file("./data/graph_structure.txt");
+    std::ifstream inputs_states_file("./data/inputs_states.txt");
+    std::ofstream output_file("./data/outputs.txt");
 
-    logicSystem.addEntryNode(input1);
-    logicSystem.addEntryNode(input2);
-    logicSystem.addEntryNode(input3);
-    logicSystem.addNode(node1);
-    logicSystem.addNode(node2);
-    logicSystem.addNode(node3);
-    logicSystem.addNode(node4);
-    logicSystem.addNode(node5);
+    if(!graph_structure_file){
+        std::cerr << "couldn't open graph structure file" << std::endl;
+    }
+    else if(!inputs_states_file){
+        std::cerr << "couldn't open inputs states file" << std::endl;
+    }
+    else if(!output_file){
+        std::cerr << "couldn't open output file" << std::endl;
+    }
+    else{
+        LogicSystem logicSystem;
 
-    logicSystem.setOutputNodeId(node5->id);
+        ENTRY* input1 = new ENTRY(1, State::HIGH, {4, 5, 6});
+        ENTRY* input2 = new ENTRY(2, State::HIGH, {4});
+        ENTRY* input3 = new ENTRY(3, State::LOW, {5, 6});
 
-    logicSystem.printConnections();
+        AND* node1 = new AND(4, {7});
+        OR* node2 = new OR(5, {7});
+        XOR* node3 = new XOR(6, {});
+        OR* node4 = new OR(7, {8});
+        NOT* node5 = new NOT(8, {});
 
-    input1->printInfo();
-    node1->printInfo();
-    node2->printInfo();
-    node3->printInfo();
-    node4->printInfo();
-    node5->printInfo();
+        logicSystem.addEntryNode(input1);
+        logicSystem.addEntryNode(input2);
+        logicSystem.addEntryNode(input3);
+        logicSystem.addNode(node1);
+        logicSystem.addNode(node2);
+        logicSystem.addNode(node3);
+        logicSystem.addNode(node4);
+        logicSystem.addNode(node5);
 
-    input1->tryResolve();
-    input2->tryResolve();
-    input3->tryResolve();
+        logicSystem.setOutputNodeId(node5->id);
 
-    node1->printInfo();
-    node2->printInfo();
-    node3->printInfo();
-    node4->printInfo();
-    node5->printInfo();
-    
-    logicSystem.printOutput();
+        logicSystem.printConnections();
+
+        input1->printInfo();
+        node1->printInfo();
+        node2->printInfo();
+        node3->printInfo();
+        node4->printInfo();
+        node5->printInfo();
+
+        input1->tryResolve();
+        input2->tryResolve();
+        input3->tryResolve();
+
+        node1->printInfo();
+        node2->printInfo();
+        node3->printInfo();
+        node4->printInfo();
+        node5->printInfo();
+        
+        logicSystem.printOutput();
+    }
+
 
     int a = static_cast<int>(State::LOW);
 
